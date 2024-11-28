04ec06b2540070f93700ad33eab525946aac7295
JuanManuelGilQuiroga <157772473+JuanManuelGilQuiroga@users.noreply.github.com>
2024-11-28 17:20:01 -0500

    feat: :sparkles: Creation of a label that react to an action listener

--- src/main/java/Forma.java
+++ src/main/java/Forma.java
@@ -1,21 +1,35 @@
+import com.formdev.flatlaf.FlatDarculaLaf;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Forma extends JFrame{
-    private JPanel panel1;
+    private JPanel panelPrincipal;
+    private JTextField campoTexto;
+    private JLabel replicadorLabel;
 
     public Forma(){
         inicializarForma();
+        campoTexto.addActionListener( e -> {
+            System.out.println("Se ejecuto action listener");
+        });
     }
 
     private void inicializarForma(){
-        setContentPane(panel1);
+        setContentPane(panelPrincipal);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(600,400);
         setLocationRelativeTo(null);//centramos la ventana
     }
 
     public static void main(String[] args) {
+        FlatDarculaLaf.setup(); // Cambiar a modo dark
         Forma forma = new Forma();
         forma.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
